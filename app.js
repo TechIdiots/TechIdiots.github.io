767306b560caac6b94377f5377256b05c152275a
TechIdiots <57003932+TechIdiots@users.noreply.github.com>
2020-03-11 12:46:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@
   const preObject = document.getElementById('object');
 
   const dbRefObject = firebase.database().ref().child('object');
-  dbRefObject.on('value',snap => console.log(snap.val()));
+  dbRefObject.on('value',snap => {
+    preObject.InnerText = JSON.stringify(snap.val());
+  });
 
 }());
