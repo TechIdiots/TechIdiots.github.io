65a1d055efb756b810ed0d43ac0c77722de32981
TechIdiots <57003932+TechIdiots@users.noreply.github.com>
2020-03-11 12:55:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
     authDomain: "mynewproje.firebaseapp.com",
     databaseURL: "https://mynewproje.firebaseio.com",
     projectId: "mynewproje",
-    storageBucket: "https://mynewproje.firebaseio.com/users/Ty88GjnFvWfAE2SyxGa7",
+    storageBucket: "https://mynewproje.appspot.com",
     messagingSenderId: "416132997388",
     appId: "1:416132997388:web:4e20066e340495ff6a2929",
     measurementId: "G-DNJYKK1XZF"
