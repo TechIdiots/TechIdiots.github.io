aa39f8a1b5dc27feb16d2ff91c19e3b66aed3c52
TechIdiots <57003932+TechIdiots@users.noreply.github.com>
2020-03-11 12:43:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+(
+  function(){
+    const config = {
+    apiKey: "AIzaSyDiSqdmoLK4X743OBdf-CBe9KEgarSO9Tg",
+    authDomain: "mynewproje.firebaseapp.com",
+    databaseURL: "https://mynewproje.firebaseio.com",
+    projectId: "mynewproje",
+    storageBucket: "https://mynewproje.firebaseio.com/users/Ty88GjnFvWfAE2SyxGa7",
+    messagingSenderId: "416132997388",
+    appId: "1:416132997388:web:4e20066e340495ff6a2929",
+    measurementId: "G-DNJYKK1XZF"
+    };
+    firebase.initializeApp(config);
+
+  const preObject = document.getElementById('object');
+
+  const dbRefObject = firebase.database().ref().child('object');
+  dbRefObject.on('value',snap => console.log(snap.val()));
+
+}());
